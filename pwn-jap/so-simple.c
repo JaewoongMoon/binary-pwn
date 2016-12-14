d7982e380fdf2037d829d9a3dcd8037d24e977ee
JaewoongMoon <mjw8585@gmail.com>
2016-12-14 11:05:30 +0900

    add

---
+++ pwn-jap/so-simple.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int zero = 2;
+
+	printf("zero: %d", zero);
+	
+
+	//printf("zero address\t= %x\n", (int)&zero);
+
+	return 0;
+}
--- pwn/0.buffer-overflow/overflow.c
+++ pwn/0.buffer-overflow/overflow.c
@@ -1,3 +1,6 @@
+#include <string.h>
+#include <stdlib.h>
+
 void overflow_function (char *str)
 {
 	char buffer[20];
@@ -7,7 +10,7 @@ void overflow_function (char *str)
 
 int main()
 {
-	char big_strting[128];
+	char big_string[128];
 	int i;
 
 	for (i=0; i< 128; i++){
